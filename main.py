28d582f3d4cdd32baeebcaccf40de91c95e8e176
Peter <dkim7800@gmail.com>
2022-09-05 20:18:42 +0900

    making changes to 'development'

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def hello():
+    print("HAHA! Hello!!")
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm Git')
+    print("HAHA")
+    hello()
